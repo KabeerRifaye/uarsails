4e39fd043ada691ef4ab221c20c21e620a191ac4
Kabeer Rifaye <kabeer.r@seocompany.com>
2015-09-01 12:42:51 +0530

    validation error in flash message

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -8,16 +8,30 @@
 module.exports = {
 
 	// This loads the signup page. --> user/new.ejs
-	'new': function(req, res){
+	new: function(req, res){
+		res.locals.flash = _.clone(req.session.flash);
 		res.view();
+		req.session.flash = {};
 	},
 
 	//Create a new user
 	create: function(req, res, next){
 		User.create(req.params.all(), function userCreated(err, user){
-			if(err) 
-				return next(err);
+
+			// If there is an error 
+			if(err){
+				console.log(err);
+				req.session.flash = {
+					err: err
+				}
+
+				//If there's an error. Redirect to registration page.
+				return res.redirect('/user/new');
+			}
+
+			// If the user is successfully created.
 			res.json(user);
+			req.session.flash = {};
 		});
 	}
 };
